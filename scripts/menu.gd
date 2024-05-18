1672e7ae1f6da0bd4c141a8d3a4a80d0a72b09d7
Alexis <alexishachemi@gmail.com>
2024-05-18 09:50:52 +0200

    Fix Lucien's stuff

--- scripts/menu.gd
+++ scripts/menu.gd
@@ -12,11 +12,11 @@ func _process(delta):
 
 
 func _on_start_button_pressed():
-	get_tree().change_scene_to_file("res://game.tscn")
+	get_tree().change_scene_to_file("res://scenes/game.tscn")
 
 
 func _on_options_button_pressed():
-	get_tree().change_scene_to_file("res://options.tscn")
+	get_tree().change_scene_to_file("res://scenes/options.tscn")
 
 
 func _on_quit_button_pressed():
