48a3520fe6a217db77bdd08f2a9bb6cf9aee919f
Karim mohamed <karimohamed09@gmail.com>
2024-05-18 17:15:40 +0200

    Add new platform and new background

--- scripts/GameCamera.gd
+++ scripts/GameCamera.gd
@@ -10,13 +10,22 @@ var camera_speed = 30  # Speed at which the camera moves upward, adjust as neede
 @onready var background3 = $Background3
 @onready var background4 = $Background4
 
+var initBG1;
+var initBG2;
+var initBG3;
+var initBG4;
+
 # Offsets for parallax scrolling
-var parallax_offsets = [-0.05, -0.1, -0.2, -0.4]
+var parallax_offsets = [-1.05, -1.1, -1.2, -1.4]
 
 # Signal connection (assuming the BeatMover emits 'beat' and the camera is a child or reachable by the BeatMover)
 func _ready():
 	beat_mover.connect("beat", _on_beat)
 	# Initialize the position of backgrounds
+	initBG1 = background1.position.y
+	initBG2 = background2.position.y
+	initBG3 = background3.position.y
+	initBG4 = background4.position.y
 	update_background_positions()
 
 func _process(delta):
@@ -26,10 +35,10 @@ func _process(delta):
 
 func update_background_positions():
 	# Update the position of each background relative to the camera
-	background1.position.y = position.y * parallax_offsets[0]
-	background2.position.y = position.y * parallax_offsets[1]
-	background3.position.y = position.y * parallax_offsets[2]
-	background4.position.y = position.y * parallax_offsets[3]
+	background1.position.y = initBG1 + position.y * parallax_offsets[0]
+	background2.position.y = initBG2 + position.y * parallax_offsets[1]
+	background3.position.y = initBG3 + position.y * parallax_offsets[2]
+	background4.position.y = initBG4 + position.y * parallax_offsets[3]
 
 func _on_beat(beats_passed):
 	# Start the zoom effect
