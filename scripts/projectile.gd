8cc3bcac725e36f3c66d11110bfbb338b76f8165
mathematisse <lucien.pineau@epitech.eu>
2024-05-18 14:28:57 +0200

    Stuff more add

---
+++ scripts/projectile.gd
@@ -0,0 +1,12 @@
+extends RigidBody2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	var notifier = VisibleOnScreenEnabler2D.new()
+	add_child(notifier)
+	notifier.connect("screen_exited", _on_ScreenExited)
+
+func _on_ScreenExited():
+	print("bye")
+	queue_free()  # Deletes the object
